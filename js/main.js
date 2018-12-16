7ff90a2a393ccb8d27d2799c33e44f3d3eaee064
dfunkydog <mrmichaeldyer@gmail.com>
2018-12-16 21:07:39 +0000

    Check if already found, update shapes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
+let available = 0;
+
+let userSet = [];
+let foundSets = [];
 document.onreadystatechange = function() {
   if (document.readyState === "interactive") {
-    Game.start();
+    while (available < 6) {
+      Game.start();
+      available = Game.sets.length;
+    }
     renderTable();
+    renderStatus();
   }
 };
 
@@ -25,22 +33,51 @@ function renderTable() {
   });
 }
 
-let userSet = [];
+function renderStatus() {
+  foundEle = document.getElementById("found");
+  availableEle = document.getElementById("available");
+  availableEle.textContent = available;
+  foundEle.textContent = foundSets.length;
+}
+
 function handleCardSelect(card) {
   if (userSet.length === 3) {
     resetSelections();
   }
   if (!card.classList.contains("active")) {
     userSet.push(getCard(card.dataset.cardId));
+  } else {
+    userSet.splice(
+      userSet.findIndex(set => {
+        return set.id == card.dataset.cardId;
+      }),
+      1
+    );
   }
+
   if (userSet.length === 3) {
     if (Game.isSet(...userSet)) {
-      console.log("you've got a set");
+      alreadyFound(userSet);
     } else {
-      console.log(Game.whyNot(...userSet));
+      flash(Game.whyNot(...userSet), `error`);
     }
   }
   card.classList.toggle("active");
+}
+/**
+ *
+ * @param {string} message
+ * @param {string} type
+ */
+function flash(message = "", type = "success") {
+  const ele = document.getElementById("feedback");
+  ele.classList.remove("error", "success", "warn");
+  ele.classList.add(type);
+  ele.textContent = `${message}`;
+  setTimeout(() => {
+    ele.classList.remove("error", "success", "warn");
+    ele.textContent = "";
+  }, 4000);
 }
 
 function resetSelections() {
@@ -65,12 +102,44 @@ function addSvg(parent, card) {
   while (counter < card.count) {
     var newSvg = document.createElement("div");
     newSvg.classList.add("shape__wrapper");
-    newSvg.innerHTML += `<svg viewBox="0 0 4.6113 14.363" class="shape ${
-      card.color
-    } ${card.fill}" >
+    newSvg.innerHTML += `<svg viewBox="0 0 8 16" class="shape ${card.color} ${
+      card.fill
+    }" >
     <use xlink:href="img/shapes.svg#${card.shape}"></use>
     </svg>`;
     parent.appendChild(newSvg);
     counter++;
   }
+}
+
+function alreadyFound(set) {
+  let isNewSet = false;
+  if (foundSets.length === 0) {
+    isNewSet = true;
+  } else {
+    isNewSet = !matchSets(set);
+  }
+
+  if (isNewSet) {
+    flash(`You've found a set!`, `success`);
+    foundSets.push(set);
+    renderStatus();
+    return true;
+  } else {
+    flash(`This set has already been found!`, `error`);
+    renderStatus();
+    return true;
+  }
+}
+function matchSets(set) {
+  let matched = false;
+  for (const found of foundSets) {
+    if (matched) continue;
+    matched = found.every(card => {
+      return set.find(item => {
+        return item.id === card.id;
+      });
+    });
+  }
+  return matched;
 }
